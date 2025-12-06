7feaf150efb8826868accd22b04e4db9a4eb080d
Your Name <aizelartunlock@gmail.com>
2025-12-07 02:49:09 +0800

    update

--- components/BackgroundBlur.tsx
+++ components/BackgroundBlur.tsx
@@ -32,14 +32,14 @@ export default function BackgroundBlur() {
       {/* Background blur effects with warping animations and parallax - only at sides */}
       {/* Left side gradient - warping with parallax */}
       <div
-        className="absolute left-[-100px] md:left-[-720px] top-[20vh] w-[400px] md:w-[800px] h-[600px] md:h-[1200px] bg-[rgba(157,255,0,0.25)] rounded-full pointer-events-none animate-gradient-warp-2"
+        className="absolute left-[-80px] md:left-[-200px] top-[20vh] w-[300px] md:w-[400px] h-[400px] md:h-[500px] bg-[rgba(157,255,0,0.3)] rounded-full pointer-events-none animate-gradient-warp-2"
         data-parallax
         data-speed="0.5"
       />
 
       {/* Right side gradient - warping variant 3 with parallax */}
       <div
-        className="absolute right-[-100px] md:right-[-720px] top-[40vh] w-[400px] md:w-[800px] h-[600px] md:h-[1200px] bg-[rgba(157,255,0,0.25)] rounded-full pointer-events-none animate-gradient-warp-3"
+        className="absolute right-[-80px] md:right-[-200px] top-[40vh] w-[300px] md:w-[450px] h-[400px] md:h-[550px] bg-[rgba(157,255,0,0.3)] rounded-full pointer-events-none animate-gradient-warp-3"
         data-parallax
         data-speed="0.4"
       />
